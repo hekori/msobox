a9adb90f96bf54aa74d3afec73c3f812b23d4e8a
Sebastian Walter <sebastian.walter@gmail.com>
2016-05-23 10:57:15 +0200

    update setup.py to exclude numpy requirement

--- setup.py
+++ setup.py
@@ -102,11 +102,11 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip"s
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=[
-        "numpy",  # used internally for all computations
-        # "scipy",  # using linear algebra routines
-        "cffi",  # to enable calls to compiled fortran derivatives
-    ],
+    # install_requires=[
+    #     "numpy",  # used internally for all computations
+    #     # "scipy",  # using linear algebra routines
+    #     "cffi",  # to enable calls to compiled fortran derivatives
+    # ],
 
     # Testing with pytest and specific settings
     tests_require=["pytest"],
